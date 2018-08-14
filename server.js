3251f49be37664f510b07ec33945fd2137a99413
Austin.Wiedenman <awiedenman@gmail.com>
2018-08-13 20:23:14 -0600

    Hook up express to localhost:3000 and console.log to ensure success

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require(express)
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Palette Picker';
+
+app.get('/', (request, response) => {
+  response.send('Palette Picker');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+});
