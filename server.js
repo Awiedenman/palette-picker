d72aca4a0db7f78e7b1f8a956220aece9ff42e97
Austin.Wiedenman <awiedenman@gmail.com>
2018-08-13 20:44:32 -0600

    Add sample code to express to test get from /api/v1/palettes/

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
@@ -7,6 +11,21 @@ app.locals.title = 'Palette Picker';
 app.get('/', (request, response) => {
   response.send('Palette Picker');
 });
+
+app.locals.palettes = [
+  {
+    projectName: 'RoadKill',
+    paletteName: 'Austin loves colors',
+    colors: ['ffffff', 'ffffff', 'ffffff', 'ffffff', 'ffffff', 'ffffff']
+
+  }
+]
+
+app.get('/api/v1/palettes/', (request, response) => {
+  const palettes = app.locals.palettes
+
+  response.json({palettes})
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
