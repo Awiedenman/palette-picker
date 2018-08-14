2622e4e1cac3250e203cc17156b61043e1584edd
Austin.Wiedenman <awiedenman@gmail.com>
2018-08-14 12:00:23 -0600

    Enable POST for palettes in express

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('./knexfile')[environment];
+// const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -8,16 +11,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
   response.send('Palette Picker');
 });
 
 app.locals.palettes = [
   {
-    projectName: 'RoadKill',
-    paletteName: 'Austin loves colors',
+    project_name: 'RoadKill',
+    palette_name: 'Austin loves colors',
     colors: ['ffffff', 'ffffff', 'ffffff', 'ffffff', 'ffffff', 'ffffff']
-
   }
 ]
 
@@ -26,6 +30,29 @@ app.get('/api/v1/palettes/', (request, response) => {
 
   response.json({palettes})
 })
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
+  app.locals.palettes.push(palette);
+
+  response.status(201).json(palette)
+  
+  // const keys = [
+  //   "project_name",
+  //   "palette_name",
+  //   "colors"
+  // ]
+  
+  // if (!palette) {
+  //   return response.status(422).send({
+  //     error: 'No palette provided'
+  //   });
+  // } else {
+  //   database('palettes').insert(palette, [...keys, 'id'])
+  //   .then(palette=> response.status(201).json(palette[0]))
+  //   .catch(error=> response.status(500).json({error}))
+  // }
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
