1a1c3ca5b68c1d06aa1df2bd3f4a88c90ced4d11
Austin.Wiedenman <awiedenman@gmail.com>
2018-08-14 15:05:45 -0600

    Put colors into array in constructor of es6 class

--- public/script.js
+++ public/script.js
@@ -1,28 +1,52 @@
-// class Colors {
-//   constructor() { 
-//     colors: {
-//       color1,
-//       color2,
-//       color3
-//     }
-//   }
+
+class Palette {
+  constructor() { 
+    this.colors = [
+      {color: null, locked: false},
+      {color: null, locked: false},
+      {color: null, locked: false},
+      {color: null, locked: false},
+      {color: null, locked: false}
+    ]
+  }  
   
-$(".palette-generate-button").on("click", randomColors);
-
-randomColors();
-
-function newColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-} 
-
-function toggleLock() {
-}
-
-function randomColors(e) {
-  for (let i = 0; i < 5; i++) {
-    var paletteColor = newColor();
-    $(`.hex-${i}`).text(paletteColor);
-    // console.log(paletteColor);
-    $(`.palette-color-${i}`).css('backgroundColor', `${paletteColor}`);
+  newColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // console.log($)
+  } 
+  
+  populateColors() {
+    this.colors.forEach(colorNum => {
+      if (!colorNum.locked){
+        colorNum.color = this.newColor()
+      }
+    })
+    // console.log(this.colors)
   }
 }
+
+
+
+  // toggleLock() {
+  //         }
+  // }
+
+  let palette = new Palette();
+             
+  createPalette();
+  $(".palette-generate-button").on("click", createPalette);
+   
+  
+  function createPalette() {
+    palette.populateColors()
+    assignRandomColors()
+  }
+
+  function assignRandomColors(e) {
+    const paletteColors = palette.colors;
+    for (let i = 0; i < 5; i++) {
+      $(`.hex-${i}`).text(paletteColors[i].color);
+      // console.log(paletteColor);
+      $(`.palette-color-${i}`).css('backgroundColor', `${paletteColors[i].color}`);
+    }
+  }
