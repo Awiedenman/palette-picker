f55d2c409f6a920dc4003441cdfab9d657044982
Austin.Wiedenman <awiedenman@gmail.com>
2018-08-16 18:11:25 -0600

    modify endpoint for palette POST

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
@@ -19,115 +17,83 @@ app.get('/', (request, response) => {
   response.send('Palette Picker');
 });
 
-app.get('/api/v1/projects/', (request, response) => {
-  database('projects').select()
-    .then((projects) => {
-      response.status(200).json(projects);
-    })
-    .catch((error) => {
-      response.status(500).json({
-        error
-      })
-    })
-})
-
-app.get('/api/v1/projects/:id', (request, response) => {
-  database('projects').where('id', request.params.id).select()
+app.get('/api/v1/projects/:project_name', (request, response) => {
+  database('projects').where('project_name', request.params.project_name).select()
     .then(projects => {
       if (projects.length) {
         response.status(200).json(projects);
       } else {
         response.status(404).json({
-          error: `Could not find project with id ${request.params.id}`
+          error: `Could not find projects with name ${request.params.project_name} fuck you, dummy`
         });
       }
     })
     .catch(error => {
-      response.status(500).json({ error })
+      response.status(500).json({
+        error
+      })
     })
+})
+
+app.get('/api/v1/projects/', (request, response) => {
+  database('projects').select()
+  .then((projects) => {
+    response.status(200).json(projects);
+  })
+  .catch((error)=> {
+    response.status(500).json({error})
+  })
 })
 
 app.get('/api/v1/palettes', (request, response) => {
   database('palettes').select()
-    .then((palettes) => {
-      response.status(200).json(palettes);
-    })
-    .catch((error) => {
-      response.status(500).json({
-        error
-      })
-    })
-})
-
-app.get('/api/v1/palettes/:id', (request, response) => {
-  database('palettes').where('palette_id', request.params.id).select()
-    .then(palettes => {
-      if (palettes.length) {
-        response.status(200).json(palettes);
-      } else {
-        response.status(404).json({
-          error: `Could not find palette with id ${request.params.id}`
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error })
-    })
+  .then((palettes) => {
+    response.status(200).json(palettes);
+  })
+  .catch((error) => {
+    response.status(500).json({error})
+  })
 })
 
 app.post('/api/v1/projects', (request, response) => {
   const project = request.body;
-
-  for (let requiredParameter of ['project_name']) {
+  
+  for (let requiredParameter of ['project_name']){
     if (!project[requiredParameter]) {
       return response
         .status(422)
-        .send({
-          error: `Expected format: { project_name: <STRING>} You're missing a "${requiredParameter}" property.`
-        });
+        .send({error: `Expected format: { project_name: <STRING>} You're missing a "${requiredParameter}" property.`});
     }
   }
 
-  database('projects').insert(project, 'id')
-    .then(project => {
-      response.status(201).json({
-        id: project[0]
-      })
-    })
-    .catch(error => {
-      response.status(500).json({
-        error
-      });
-    })
+database('projects').insert(project, 'id')
+  .then(project => {
+    response.status(201).json({ id: project[0]})
+  })
+  .catch(error => {
+    response.status(500).json({error});
+  })
 });
 
-
-app.post('/api/v1/palettes/', (request, response) => {
+  app.post('/api/v1/palettes', (request, response) => {
   const palette = request.body;
-
+  
   for (let requiredParameter of ['palette_name', 'color_1', 'color_2', 'color_3', 'color_4', 'color_5', 'project_id']) {
     if (!palette[requiredParameter]) {
       return response
         .status(422)
-        .send({
-          error: `Expected format: { project_name: <STRING>} You're missing a "${requiredParameter}" property.`
-        })
+        .send({ error: `Expected format: { project_name: <STRING>} You're missing a "${requiredParameter}" property.`})
     }
   }
-
-  database('palettes').insert(palette, 'palette_id')
-    .then(palette => {
-      response.status(201).json({
-          id: palette[0]
-        })
-        .catch(error => {
-          response.status(500).json({
-            error
-          });
-        })
-    });
-})
+  
+    database('palettes').insert(palette, 'palette_id')
+      .then(palette=> {
+    response.status(201).json(palette[0])})
+    .catch(error=> {
+      response.status(500).json({error});
+  })
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
-})
+});
